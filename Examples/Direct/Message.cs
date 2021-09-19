76540fdc3086b46f0129f718a65a6e14399d8b80
Sergei.Chechin <Sergei.Chechin@itl.team>
2021-09-19 17:07:33 +0300

    Add example for direct manager

---
+++ Examples/Direct/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace Direct
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int Index { get; set; }
+    }
+}
---
+++ Examples/Direct/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Workflow.Builders;
+
+namespace Direct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new Generator(1, 5, 20);
+            var fork = new DirectWorkflowBuilder<Message>(x => x.Id, Handle)
+                .SetWorkersCapacity(5)
+                .Build();
+
+            generator.OnGenerated += (s, m) =>
+            {
+                return fork.PostAsync(m);
+            };
+
+            generator.Start(5000);
+
+            Console.ReadKey();
+        }
+
+        private static Task Handle(Message msg)
+        {
+            var rnd = new Random();
+
+            Console.WriteLine($"Received message: {msg.Id} index: {msg.Index}");
+
+            return Task.Delay(rnd.Next(900, 1100));
+        }
+    }
+}
